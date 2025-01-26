a51db1940ee113753caaa1aec3487ca4a7d76b91
Steven <stevendotpulido@gmail.com>
2025-01-26 01:12:52 -0800

    refactor: add bg styles

--- src/components/Banner/AboutBanner.tsx
+++ src/components/Banner/AboutBanner.tsx
@@ -1,8 +1,11 @@
 export default function AboutBanner({ image, blur }: { image: string; blur: string }) {
     return (
         <div
-            className={`lg:w-[1160px] transition duration-500 ease-in-out hover:opacity-80 h-210 overflow-hidden rounded-xl bg-[url(/${image})] bg-cover bg-center`}
-            style={{ filter: `blur(${blur})` }}
+            className={`lg:w-[1160px] transition duration-500 ease-in-out hover:opacity-80 h-210 overflow-hidden rounded-xl`}
+            style={{
+                filter: `blur(${blur})`,
+                background: `url(/${image}) no-repeat center center / cover`
+            }}
         ></div>
     );
 }
