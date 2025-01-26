00fdb4fb77d0c8ee955a21d2374076669917c576
Steven <stevendotpulido@gmail.com>
2025-01-25 23:44:58 -0800

    chore: remove AnimatePresence import

--- src/views/login/Login.tsx
+++ src/views/login/Login.tsx
@@ -1,5 +1,5 @@
 import { useRef } from "react";
-import { motion, AnimatePresence } from "motion/react";
+import { motion } from "motion/react";
 import { MdLogin } from "react-icons/md";
 
 export default function Login() {
