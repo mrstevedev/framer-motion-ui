eb85b6747b71dbbec18c16fff464e72ee337f1f0
Steven <stevendotpulido@gmail.com>
2025-01-26 00:58:49 -0800

    refactor: remove image for background image

--- src/components/Banner/AboutBanner.tsx
+++ src/components/Banner/AboutBanner.tsx
@@ -1,8 +1,8 @@
 export default function AboutBanner({ image, blur }: { image: string; blur: string }) {
-    console.log("blur", blur);
     return (
-        <div className={`lg:w-[1160px] transition duration-500 ease-in-out hover:opacity-80 h-210 overflow-hidden rounded-xl`}>
-            <img src={image} alt="sd steakhouse" className={`w-full h-full object-cover`} style={{ filter: `blur(${blur})` }} />
-        </div>
+        <div
+            className={`lg:w-[1160px] transition duration-500 ease-in-out hover:opacity-80 h-210 overflow-hidden rounded-xl bg-[url(/${image})] bg-cover bg-center`}
+            style={{ filter: `blur(${blur})` }}
+        ></div>
     );
 }
