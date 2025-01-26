4de74324d94be6d003e44dd8b03a417a82e2465f
Steven <stevendotpulido@gmail.com>
2025-01-26 00:26:38 -0800

    chore: add log

--- src/components/Banner/AboutBanner.tsx
+++ src/components/Banner/AboutBanner.tsx
@@ -1,4 +1,5 @@
 export default function AboutBanner({ image, blur }: { image: string; blur: string }) {
+    console.log("blur", blur);
     return (
         <div className={`lg:w-[1160px] transition duration-500 ease-in-out hover:opacity-80 h-210 overflow-hidden rounded-xl`}>
             <img src={image} alt="sd steakhouse" className={`w-full h-full object-cover blur-[${blur}]`} />
