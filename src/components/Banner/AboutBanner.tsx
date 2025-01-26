6644bc5f5c092cc948ff6c55cc0a9fd19711316b
Steven <stevendotpulido@gmail.com>
2025-01-26 00:17:59 -0800

    refactor: move blur to img

--- src/components/Banner/AboutBanner.tsx
+++ src/components/Banner/AboutBanner.tsx
@@ -1,9 +1,7 @@
 export default function AboutBanner({ image, blur }: { image: string; blur: string }) {
     return (
-        <div
-            className={`lg:w-[1160px] blur-[${blur}] transition duration-500 ease-in-out hover:opacity-80 h-210 overflow-hidden rounded-xl`}
-        >
-            <img src={image} alt="" className="w-full h-full object-cover" />
+        <div className={`lg:w-[1160px] transition duration-500 ease-in-out hover:opacity-80 h-210 overflow-hidden rounded-xl`}>
+            <img src={image} alt="sd steakhouse" className={`w-full h-full object-cover blur-[${blur}]`} />
         </div>
     );
 }
