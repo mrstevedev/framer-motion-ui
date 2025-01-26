3dbb265dd109b53da0bf1271fe385f4aaa8d0e07
Steven <stevendotpulido@gmail.com>
2025-01-26 00:11:06 -0800

    chore: update to image element

--- src/components/Banner/AboutBanner.tsx
+++ src/components/Banner/AboutBanner.tsx
@@ -1,8 +1,9 @@
 export default function AboutBanner({ image, blur }: { image: string; blur: string }) {
-    console.log("blur", blur);
     return (
         <div
-            className={`lg:w-[1160px] blur-[${blur}] transition duration-500 ease-in-out hover:opacity-80 h-210 overflow-hidden rounded-xl bg-[url(/${image})] bg-cover bg-center relative`}
-        ></div>
+            className={`lg:w-[1160px] blur-[${blur}] transition duration-500 ease-in-out hover:opacity-80 h-230 overflow-hidden rounded-xl`}
+        >
+            <img src={image} alt="" className="w-full h-full object-cover" />
+        </div>
     );
 }
